c6769550b5c1f722ffc5eb0908f587b69203347b
Maniutiu Raul <raul.maniutiu@yahoo.com>
2020-10-29 21:28:36 +0200

    Add angular material Update steppser component

--- frontend/src/app/pages/layout/layout.module.ts
+++ frontend/src/app/pages/layout/layout.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatSliderModule } from '@angular/material/slider';
+
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { StepperComponent } from './stepper/stepper.component';
-
+import { MaterialModule } from '../../shared/material.module'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LayoutComponent, StepperComponent],
   imports: [
     CommonModule,
-    LayoutRoutingModule
+    LayoutRoutingModule,
+    MatSliderModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LayoutModule { }
