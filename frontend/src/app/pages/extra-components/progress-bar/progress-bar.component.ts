10e73bac158d105364590db1d0bf4351e27120d9
Maniutiu Raul <raul.maniutiu@yahoo.com>
2020-11-04 20:21:48 +0200

    Add Interactive progressBar

--- frontend/src/app/pages/extra-components/progress-bar/progress-bar.component.ts
+++ frontend/src/app/pages/extra-components/progress-bar/progress-bar.component.ts
@@ -8,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class ProgressBarComponent implements OnInit {
 
   height = '20px';
+  value = 25;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  subtract() {
+    if (this.value >= 25) {
+      this.value -= 25;
+    }
+  }
+
+  add() {
+    if (this.value <= 75) {
+      this.value += 25;
+    }
+  }
 }
