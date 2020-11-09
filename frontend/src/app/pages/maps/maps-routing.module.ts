bccc423e5473d70222eb3d722b5b7d2e0659862c
Maniutiu Raul <raul.maniutiu@yahoo.com>
2020-11-09 18:03:56 +0200

    Create leaflet component

--- frontend/src/app/pages/maps/maps-routing.module.ts
+++ frontend/src/app/pages/maps/maps-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GoogleMapsComponent } from './google-maps/google-maps.component';
+import { LeafletComponent } from './leaflet/leaflet.component';
 import { MapsComponent } from './maps.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MapsComponent,
-    children: [{
-      path: 'gmaps',
-      component: GoogleMapsComponent
-    }]
+    children: [
+      {
+        path: 'gmaps',
+        component: GoogleMapsComponent
+      },
+      {
+        path: 'leaflet',
+        component: LeafletComponent
+      }
+    ]
   }
 ];
 
--- frontend/src/app/pages/maps/maps.module.ts
+++ frontend/src/app/pages/maps/maps.module.ts
@@ -5,20 +5,22 @@ import { MapsRoutingModule } from './maps-routing.module';
 import { GoogleMapsComponent } from './google-maps/google-maps.component';
 import { CoreModule } from '../../core/core.module'
 import { MaterialModule } from '../../shared/material.module';
+
 import { MapsComponent } from './maps.component';
-import { GoogleMapsModule } from '@angular/google-maps'
+import { GoogleMapsModule } from '@angular/google-maps';
+
+import { LeafletComponent } from './leaflet/leaflet.component'
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 @NgModule({
-  declarations: [GoogleMapsComponent, MapsComponent],
+  declarations: [GoogleMapsComponent, MapsComponent, LeafletComponent],
   imports: [
     CommonModule,
     MapsRoutingModule,
     CoreModule,
     MaterialModule,
-    GoogleMapsModule
-    // AgmCoreModule.forRoot({
-    //   apiKey: 'AIzaSyByJno3Qsb8n9uIIM1NX8fBkbAJPvWDt6Q'
-    // })
+    GoogleMapsModule,
+    LeafletModule
   ]
 })
 export class MapsModule { }
