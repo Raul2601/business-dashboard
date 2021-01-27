49998646dd2b6886680753b9ee04a0fbe998242c
Maniutiu Raul <raul.maniutiu@yahoo.com>
2021-01-27 22:48:59 +0200

    Add dorting and filtering to ng-table

--- frontend/src/app/pages/inventory/ng-table/ng-table.component.ts
+++ frontend/src/app/pages/inventory/ng-table/ng-table.component.ts
@@ -1,19 +1,21 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTable } from '@angular/material/table';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogBoxComponent } from '../dialog-box/dialog-box.component';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 export interface UsersData {
-  name: string;
   id: number;
+  name: string;
 }
 
 const ELEMENT_DATA: UsersData[] = [
   { id: 1560608769632, name: 'Artificial Intelligence' },
   { id: 1560608796014, name: 'Machine Learning' },
   { id: 1560608787815, name: 'Robotic Process Automation' },
-  { id: 1560608805101, name: 'Blockchain' }
+  { id: 1560608805101, name: 'Blockchain' },
 ];
 
 @Component({
@@ -21,12 +23,25 @@ const ELEMENT_DATA: UsersData[] = [
   templateUrl: './ng-table.component.html',
   styleUrls: ['./ng-table.component.css']
 })
-export class NgTableComponent {
+export class NgTableComponent implements AfterViewInit {
 
   displayedColumns: string[] = ['id', 'name', 'action'];
-  dataSource = ELEMENT_DATA;
+  dataSource = new MatTableDataSource<UsersData>(ELEMENT_DATA);
 
   @ViewChild(MatTable, { static: true }) table: MatTable<any>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 
   constructor(public dialog: MatDialog) { }
 
@@ -50,23 +65,25 @@ export class NgTableComponent {
 
   addRowData(row_obj) {
     var d = new Date();
-    this.dataSource.push({
+    var data = this.dataSource.data;
+    data.push({
       id: d.getTime(),
       name: row_obj.name
     });
-    this.table.renderRows();
-
+    this.dataSource.data = data;
   }
+
   updateRowData(row_obj) {
-    this.dataSource = this.dataSource.filter((value, key) => {
+    this.dataSource.data = this.dataSource.data.filter((value, key) => {
       if (value.id == row_obj.id) {
         value.name = row_obj.name;
       }
       return true;
     });
   }
+
   deleteRowData(row_obj) {
-    this.dataSource = this.dataSource.filter((value, key) => {
+    this.dataSource.data = this.dataSource.data.filter((value, key) => {
       return value.id != row_obj.id;
     });
   }
