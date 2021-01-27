f8dcb7030e0cee2171ec9a5aead2961ec6e5aa74
Maniutiu Raul <raul.maniutiu@yahoo.com>
2021-01-27 23:30:12 +0200

    Create students table

--- frontend/src/app/pages/inventory/table/table.component.ts
+++ frontend/src/app/pages/inventory/table/table.component.ts
@@ -1,62 +1,116 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatSort } from '@angular/material/sort';
+
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css'],
+  styleUrls: ['./table.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class TableComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'price', 'stock', 'active', 'getdetails'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+export class TableComponent {
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  isTableExpanded = false;
 
-  isExpansionDetailRow = (index, row) => row.hasOwnProperty('detailRow');
+  STUDENTS_DATA = [
+    {
+      "id": 1,
+      "name": "Abby Jaskolski ",
+      "age": 21,
+      "address": 1.0079,
+      "isExpanded": false,
+      "subjects": [
+        {
+          "name": "Bio",
+          "type": "Medical",
+          "grade": "A"
+        },
+        {
+          "name": "Chemistry",
+          "type": "Medical",
+          "grade": "A"
+        },
+        {
+          "name": "Physics",
+          "type": "Medical",
+          "grade": "A"
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Jabari Fritsch",
+      "age": 20,
+      "address": 1.0079,
+      "isExpanded": false,
+      "subjects": [
+        {
+          "name": "Bio",
+          "type": "Medical",
+          "grade": "A"
+        },
+        {
+          "name": "Chemistry",
+          "type": "Medical",
+          "grade": "A"
+        },
+        {
+          "name": "Physics",
+          "type": "Medical",
+          "grade": "A"
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "name": "Maybell Simonis",
+      "age": 21,
+      "address": 1.0079,
+      "isExpanded": false,
+      "subjects": [
+        {
+          "name": "Bio",
+          "type": "Medical",
+          "grade": "A"
+        },
+        {
+          "name": "Chemistry",
+          "type": "Medical",
+          "grade": "A"
+        },
+        {
+          "name": "Physics",
+          "type": "Medical",
+          "grade": "A"
+        }
+      ]
+    }
+  ];
+
+
+  dataStudentsList = new MatTableDataSource();
+  displayedStudentsColumnsList: string[] = ['id', 'name', 'age', 'address', 'actions'];
+
 
   ngOnInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.dataStudentsList.data = this.STUDENTS_DATA;
   }
 
-  doFilter(filterValue: string) {
-    filterValue = filterValue.trim(); // Remove whitespace
-    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
-    this.dataSource.filter = filterValue;
+  // Toggel Rows
+  toggleTableRows() {
+    this.isTableExpanded = !this.isTableExpanded;
+
+    this.dataStudentsList.data.forEach((row: any) => {
+      row.isExpanded = this.isTableExpanded;
+    })
   }
 }
 
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  price: number;
-  stock: number;
-  active: boolean;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', price: 1.0079, stock: 12, active: false },
-  { position: 2, name: 'Helium', price: 4.0026, stock: 31, active: true },
-  { position: 3, name: 'Lithium', price: 6.941, stock: 36, active: false },
-  { position: 4, name: 'Beryllium', price: 9.0122, stock: 35, active: true },
-  { position: 5, name: 'Boron', price: 10.811, stock: 17, active: true },
-  { position: 6, name: 'Carbon', price: 12.0107, stock: 25, active: false },
-  { position: 7, name: 'Nitrogen', price: 14.0067, stock: 10, active: true },
-  { position: 8, name: 'Oxygen', price: 15.9994, stock: 15, active: true },
-  { position: 9, name: 'Fluorine', price: 18.9984, stock: 11, active: false },
-  { position: 10, name: 'Neon', price: 20.1797, stock: 8, active: true },
-  { position: 11, name: 'Sodium', price: 22.9897, stock: 18, active: true },
-  { position: 12, name: 'Magnesium', price: 24.305, stock: 12, active: false },
-  { position: 13, name: 'Aluminum', price: 26.9815, stock: 22, active: false },
-  { position: 14, name: 'Silicon', price: 28.0855, stock: 28, active: true },
-  { position: 15, name: 'Phosphorus', price: 30.9738, stock: 50, active: true },
-  { position: 16, name: 'Sulfur', price: 32.065, stock: 60, active: true },
-  { position: 17, name: 'Chlorine', price: 35.453, stock: 4, active: true },
-  { position: 18, name: 'Argon', price: 39.948, stock: 10, active: true },
-  { position: 19, name: 'Potassium', price: 39.0983, stock: 10, active: true },
-  { position: 20, name: 'Calcium', price: 40.078, stock: 10, active: false },
-];
